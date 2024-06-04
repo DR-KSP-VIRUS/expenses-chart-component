ca3c99ab8cca75c0c64c254f91bcbb40521a6778
KONJA KUMA SAMPSON <nkumasampson@gmail.com>
2024-06-04 14:40:57 -0700

    moving to meet data

--- src/apis/graphApi.js
+++ src/apis/graphApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchData = async () => {
     try {
-        const res = await axios.get("../../db/data.json");
+        const res = await axios.get("/db/data.json");
         return await res.data;
     } catch (error) {
         console.log(error);
